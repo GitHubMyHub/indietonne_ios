fcb59a97900fb424e22ea8d9f1309f80141b3e7a
Sizzler <carlos_alves@hotmail.de>
2026-03-16 16:14:38 +0100

    Initial Commit

---
+++ apple/appleApp.swift
@@ -0,0 +1,17 @@
+//
+//  appleApp.swift
+//  apple
+//
+//  Created by Doom Mortal on 16.03.26.
+//
+
+import SwiftUI
+
+@main
+struct appleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ appleTests/appleTests.swift
@@ -0,0 +1,17 @@
+//
+//  appleTests.swift
+//  appleTests
+//
+//  Created by Doom Mortal on 16.03.26.
+//
+
+import Testing
+@testable import apple
+
+struct appleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
